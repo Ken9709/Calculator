cc7b4e5cc72faa41dd51ad8eefd06200efa59979
Ken9709 <Kwood9709@gmail.com>
2022-08-24 22:45:38 -0700

    intial button setup and basic functions

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+function add(x, y)
+    {
+        return x + y;
+    }
+
+function subtract(x, y)
+    {
+        return x - y;
+    }
+
+function multiply(x, y)
+    {
+        return x*y;
+    }
+
+function divide(x,y)
+    {
+        return x/y;
+    }
+
+console.log(add(1,2));
+console.log(subtract(6,3));
+console.log(multiply(3,1));
+console.log(divide(9,3));
